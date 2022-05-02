9492f280a94d60bf7edf8b31c2a17ce13cb0fbc7
Rafa <rafa@Rafas-MacBook-Air.local>
2022-05-02 18:33:20 +0900

    serve frontend from express

--- backend/app.js
+++ backend/app.js
@@ -33,18 +33,25 @@ app.use(express.static(path.join(__dirname, 'public')))
 // CORS setting
 app.use(cors({ origin: true, credentials: true }))
 
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000') // update to match the domain you will make the request from
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept'
-//   )
-//   next()
-// })
-
+// Routes
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/codes', codesRouter)
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Party-Invitation API' })
+  })
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
